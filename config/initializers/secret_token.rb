e170bed7579aa6c7ff9ef55de20e15ba6a959337
vagrant <vagrant@precise32.(none)>
2015-08-28 19:30:36 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Funappforquiztwo::Application.config.secret_key_base = '3323b75202a15dce10e5fa0f7e0aa1c4a0a3bf304e34d553fa1796d3c84f50278bb5e50bae7d8640922a18eedea154b5bca2f4fc11ef3f133e49b01aa406a3bc'
